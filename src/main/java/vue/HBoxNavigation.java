82e7475de6620f364bb1113a36877bf7eba8c0f4
p6nj <breee@duck.com>
2023-03-21 10:55:27 +0100

    Non fonctional left and right buttons

--- src/main/java/vue/HBoxNavigation.java
+++ src/main/java/vue/HBoxNavigation.java
@@ -1,12 +1,15 @@
 package vue;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 
 public class HBoxNavigation extends HBox {
   public HBoxNavigation() {
     super();
-    Button testButton = new Button("what");
-    getChildren().addAll(testButton);
+    setAlignment(Pos.CENTER_RIGHT);
+    Button prev = new Button("<");
+    Button next = new Button(">");
+    getChildren().addAll(prev, next);
   }
 }
