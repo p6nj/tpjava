45963f90a1ac2d0fb4bd9962f5a5a6eb1cf3b34d
p6nj <breee@duck.com>
2023-04-04 10:52:51 +0200

    form base

--- src/main/java/vue/GridPaneFormulaireRéservation.java
+++ src/main/java/vue/GridPaneFormulaireRéservation.java
@@ -1,9 +1,14 @@
 package vue;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GridPaneFormulaireRéservation extends GridPane {
     public GridPaneFormulaireRéservation() {
         super();
+        Label lcours = new Label("cours");
+        Label lniveau = new Label("niveau");
+        Label lhoraire = new Label("horaire");
+        add(lcours, 1, 0);
     }
 }
